64ff610301c898faccf39e94ff39add751f39697
Fareya Hossain <fareya.hossain@g.bracu.ac.bd>
2025-09-02 06:52:29 -0400

    fourth commit

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,15 @@ use App\Mail\StaffNotificationMail;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// handle add staff
 Route::get('/', function () {
     return view('welcome',['posts'=>post::all()]);
 })->name('home');
 
 Route::get('/create', [postController::class, 'create']);
-
 Route::post('/store', [postController::class, 'ourfilestore'])->name('store');
 
-//Route::get('/edit', [postController::class, 'editData'])->name('edit');
+Route::get('/edit', [postController::class, 'editData'])->name('edit');
 
 // Show the edit form for a specific staff
 Route::get('/edit/{id}', [postController::class, 'editData'])->name('edit');
@@ -43,8 +42,11 @@ Route::delete('/delete/{id}', [PostController::class, 'deleteData'])->name('dele
 
 Route::get('/leave-request', [LeaveRequestController::class, 'create'])->name('leave.create');
 Route::post('/leave-request', [LeaveRequestController::class, 'store'])->name('leave.store');
-
 Route::get('/leave/manage', [LeaveRequestController::class, 'index'])->name('leave.index');
+
+Route::get('/leave/approve/{id}', [LeaveRequestController::class, 'approve'])->name('leave.approve');
+Route::get('/leave/deny/{id}', [LeaveRequestController::class, 'deny'])->name('leave.deny');
+
 Route::put('/leave/approve/{id}', [LeaveRequestController::class, 'approve'])->name('leave.approve');
 Route::put('/leave/deny/{id}', [LeaveRequestController::class, 'deny'])->name('leave.deny');
 
